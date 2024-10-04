d9a72686d5def2c1639cfe7745f1023918c90a75
dragmine <dan@thebanners.uk>
2024-10-04 16:36:06 +0100

    Updated how the plugin settings are handled

--- plugin.gd
+++ plugin.gd
@@ -1,6 +1,7 @@
 @tool
 extends EditorPlugin
 
+const setting_path := "addons/InputManager/";
 var export_plugin: InputManagerExportPlugin;
 
 func _enter_tree() -> void:
@@ -8,39 +9,12 @@ func _enter_tree() -> void:
 	export_plugin = InputManagerExportPlugin.new();
 	add_export_plugin(export_plugin);
 
-	if not ProjectSettings.has_setting("addons/InputManager/export_debug"):
-		ProjectSettings.set_setting("addons/InputManager/export_debug", false);
-		ProjectSettings.set_initial_value("addons/InputManager/export_debug", false);
-	ProjectSettings.add_property_info({
-		"name": "addons/InputManager/export_debug",
-		"type": TYPE_BOOL,
-	})
+	create_project_settings("export_debug", false, TYPE_BOOL);
+	create_project_settings("input_count", 2, TYPE_INT, PROPERTY_HINT_RANGE, "1,2");
+	create_project_settings("save_path", "user://inputs.tres", TYPE_STRING);
+	create_project_settings("verbose", false, TYPE_BOOL);
+	create_project_settings("icons/mouse", false, TYPE_BOOL);
 
-	if not ProjectSettings.has_setting("addons/InputManager/input_count"):
-		ProjectSettings.set_setting("addons/InputManager/input_count", 2);
-		ProjectSettings.set_initial_value("addons/InputManager/input_count", 1);
-	ProjectSettings.add_property_info({
-		"name": "addons/InputManager/input_count",
-		"type": TYPE_INT,
-		"hint": PROPERTY_HINT_RANGE,
-		"hint_string": "1,2"
-	});
-
-	if not ProjectSettings.has_setting("addons/InputManager/save_path"):
-		ProjectSettings.set_setting("addons/InputManager/save_path", "user://inputs.tres");
-		ProjectSettings.set_initial_value("addons/InputManager/save_path", "user://inputs.tres");
-	ProjectSettings.add_property_info({
-		"name": "addons/InputManager/save_path",
-		"type": TYPE_STRING
-	});
-
-	if not ProjectSettings.has_setting("addons/InputManager/verbose"):
-		ProjectSettings.set_setting("addons/InputManager/verbose", false);
-		ProjectSettings.set_initial_value("addons/InputManager/verbose", false);
-	ProjectSettings.add_property_info({
-		"name": "addons/InputManager/verbose",
-		"type": TYPE_BOOL
-	});
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("InputManager");
@@ -53,3 +27,18 @@ func _disable_plugin() -> void:
 	ProjectSettings.set_setting("addons/InputManager/input_count", null);
 	ProjectSettings.set_setting("addons/InputManager/save_path", null);
 	ProjectSettings.set_setting("addons/InputManager/verbose", null);
+	ProjectSettings.set_setting("addons/InputManager/icons/mouse", null);
+
+
+func create_project_settings(setting_name: String, setting_default_value: Variant, setting_type: Variant.Type,
+			setting_hint: PropertyHint = PROPERTY_HINT_NONE, setting_hint_string: String = "") -> void:
+	var setting_full_path = setting_path + setting_name;
+	if not ProjectSettings.has_setting(setting_full_path):
+		ProjectSettings.set_setting(setting_full_path, setting_default_value);
+		ProjectSettings.set_initial_value(setting_full_path, setting_default_value);
+	ProjectSettings.add_property_info({
+		"name": setting_full_path,
+		"type": setting_type,
+		"hint": setting_hint,
+		"hint_string": setting_hint_string
+	});
